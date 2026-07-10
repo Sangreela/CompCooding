65e7b0ad9fe519c591ce832a947eff11618aff80
Sangreela <41299301+Sangreela@users.noreply.github.com>
2026-07-11 02:31:58 +0530

    Time: 0 ms (100%), Space: 15.9 MB (7.42%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i,index=-1;
+        for(i=nums.size()-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index == -1)
+            reverse(nums.begin(),nums.end());
+        else{
+            for(i=nums.size()-1;i>index;i--)
+            {
+                if(nums[i]>nums[index]){
+                    swap(nums[index],nums[i]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+index+1,nums.end());
+        }
+    }
+};
