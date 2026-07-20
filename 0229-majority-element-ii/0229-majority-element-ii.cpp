9b9d13edad9385a4125cdca58ea92a72f3a1a6a5
Sangreela <41299301+Sangreela@users.noreply.github.com>
2026-07-20 19:22:58 +0530

    Time: 0 ms (100%), Space: 23.5 MB (85.29%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int count1=0,count2=0,element1=INT_MIN,element2=INT_MIN;
+        vector<int>res;
+        if(nums.size()<2)
+            return nums;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count1==0 && element2!=nums[i])
+            {
+                element1=nums[i];
+                count1++;
+            }
+            else if(count2==0 && element1!=nums[i]){
+                element2=nums[i];
+                count2++;
+            }
+            else if(element1==nums[i])
+                count1++;
+            else if(element2==nums[i])
+                count2++;
+            else
+                count1--,count2--;
+        }
+
+        count1=0,count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==element1)
+                count1++;
+            if(nums[i]==element2)
+                count2++;
+        }
+
+        if(count1>nums.size()/3)
+            res.push_back(element1);
+        if(count2>nums.size()/3)
+            res.push_back(element2);
+        return res;
+    }
+};
