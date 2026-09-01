814ee5045f5f87a2a31fd5e1bb7798d37cb70e2b
Sangreela <41299301+Sangreela@users.noreply.github.com>
2026-09-02 01:39:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        for(int i=0;i<nums.size();i++)
+        int i,j = 0;
+        for(i=0;i<nums.size();i++)
         {
             if(nums[i])
             {
